d39655f40dbafd89847605062188356b2c5fb180
MANJUNATH <apple@MANJUNATH.local>
2026-06-09 23:31:34 +0530

    Updated App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
-import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
+
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
-import Footer from "./components/Footer/Footer";
-import LoginPopup from "./components/LoginPopup/LoginPopup";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Verify from "./pages/Verify/Verify";
 import MyOrders from "./pages/MyOrders/MyOrders";
 
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
-      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+
       <div className="app">
         <ToastContainer />
+
         <Navbar setShowLogin={setShowLogin} />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
@@ -27,6 +34,7 @@ const App = () => {
           <Route path="/myorders" element={<MyOrders />} />
         </Routes>
       </div>
+
       <Footer />
     </>
   );
